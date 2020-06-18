7a33242ba1a0da5545b6327a2246253914576251
kowal <kowalstudia@protonmail.com>
2020-06-18 15:21:56 +0200

    ADMIN Pages v1

---
+++ Models/ViewModels/Pages/PageVM.cs
@@ -0,0 +1,33 @@
+﻿using CmsShop.Models.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace CmsShop.Models.ViewModels.Pages
+{
+    public class PageVM
+    {
+        public PageVM()
+        {
+
+        }
+        public PageVM(PageDTO row)
+        {
+            Id = row.Id;
+            Title = row.Title;
+            Slug = row.Slug;
+            Body = row.Body;
+            Sorting = row.Sorting;
+            HasSideBar = row.HasSideBar;
+            
+        }
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50,MinimumLength =3)]
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        public string Body { get; set; }
+        [Required]
+        [StringLength(int.MaxValue, MinimumLength = 3)]
+        public int Sorting { get; set; }
+        public bool HasSideBar { get; set; }
+    }
+}
